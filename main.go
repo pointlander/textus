c374edb7c303eb6335f13c9ebdb13216ac891b8e
Andrew Snodgrass <pointlander@gmail.com>
2025-05-14 16:22:11 -0500

    Added db generation

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 	"compress/bzip2"
 	"embed"
 	"io"
+	"math"
+	"os"
 )
 
 //go:embed books/*
@@ -25,11 +27,38 @@ func main() {
 		panic(err)
 	}
 
+	db, err := os.Create("db.bin")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
 	m := NewFiltered()
 	m.Add(0)
+	buffer32, buffer8 := make([]byte, 4), make([]byte, 1)
 	for _, v := range data {
 		vector := m.Mix()
-		_ = vector
+		for _, v := range vector {
+			bits := math.Float32bits(v)
+			for i := range buffer32 {
+				buffer32[i] = byte((bits >> (8 * i)) & 0xFF)
+			}
+			n, err := db.Write(buffer32)
+			if err != nil {
+				panic(err)
+			}
+			if n != len(buffer32) {
+				panic("4 bytes should be been written")
+			}
+		}
+		buffer8[0] = v
+		n, err := db.Write(buffer8)
+		if err != nil {
+			panic(err)
+		}
+		if n != 1 {
+			panic("1 byte should be been written")
+		}
 		m.Add(v)
 	}
 }
