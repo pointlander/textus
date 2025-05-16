33d96826108ac96acc4e9a95572983e2a2a754d9
Andrew Snodgrass <pointlander@gmail.com>
2025-05-15 20:41:39 -0500

    Added new mode

--- main.go
+++ main.go
@@ -28,11 +28,11 @@ var Data embed.FS
 var (
 	// FlagPrompt prompt for the model
 	FlagPrompt = flag.String("prompt", "", "prompt for the model")
+	// FlagMach1 mach 1 mode
+	FlagMach1 = flag.Bool("mach1", false, "mach 1 mode")
 )
 
-func main() {
-	flag.Parse()
-
+func Mach1() {
 	if *FlagPrompt != "" {
 		m := NewFiltered()
 		for _, v := range []byte(*FlagPrompt) {
@@ -223,6 +223,84 @@ func main() {
 		if n != 1 {
 			panic("1 byte should be been written")
 		}
+		m.Add(v)
+	}
+}
+
+func main() {
+	flag.Parse()
+
+	if *FlagMach1 {
+		Mach1()
+		return
+	}
+
+	file, err := Data.Open("books/100.txt.utf-8.bz2")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	reader := bzip2.NewReader(file)
+	data, err := io.ReadAll(reader)
+	if err != nil {
+		panic(err)
+	}
+
+	db, err := os.Create("db.bin")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	index, err := os.Create("index.bin")
+	if err != nil {
+		panic(err)
+	}
+	defer index.Close()
+
+	m := NewFiltered()
+	m.Add(0)
+	buffer32, buffer8 := make([]byte, 4), make([]byte, 1)
+	for i, v := range data {
+		vector := m.Mix()
+		for _, v := range vector {
+			bits := math.Float32bits(v)
+			for i := range buffer32 {
+				buffer32[i] = byte((bits >> (8 * i)) & 0xFF)
+			}
+			n, err := db.Write(buffer32)
+			if err != nil {
+				panic(err)
+			}
+			if n != len(buffer32) {
+				panic("4 bytes should be been written")
+			}
+		}
+		buffer8[0] = v
+		n, err := db.Write(buffer8)
+		if err != nil {
+			panic(err)
+		}
+		if n != 1 {
+			panic("1 byte should be been written")
+		}
+
+		if (i+128)%256 == 0 {
+			for _, v := range vector {
+				bits := math.Float32bits(v)
+				for i := range buffer32 {
+					buffer32[i] = byte((bits >> (8 * i)) & 0xFF)
+				}
+				n, err := index.Write(buffer32)
+				if err != nil {
+					panic(err)
+				}
+				if n != len(buffer32) {
+					panic("4 bytes should be been written")
+				}
+			}
+		}
+
 		m.Add(v)
 	}
 }
