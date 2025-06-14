cf9c7d033f7aa3d5f5b4ae84740a8ad29415cc20
Andrew Snodgrass <pointlander@gmail.com>
2025-06-14 05:25:00 -0500

    Added 64 bit matrix

--- mat64/matrix.go
+++ mat64/matrix.go
@@ -2,13 +2,11 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package main
+package mat64
 
 import (
 	"fmt"
 	"math"
-
-	"github.com/pointlander/textus/vector"
 )
 
 const (
@@ -21,23 +19,31 @@ const (
 	S = 1.0 - 1e38*math.SmallestNonzeroFloat32
 )
 
-// Matrix is a float32 matrix
+// Matrix is a float64 matrix
 type Matrix struct {
 	Cols int
 	Rows int
-	Data []float32
+	Data []float64
 }
 
-// NewMatrix creates a new float32 matrix
-func NewMatrix(cols, rows int, data ...float32) Matrix {
+// NewMatrix creates a new float64 matrix
+func NewMatrix(cols, rows int, data ...float64) Matrix {
 	if data == nil {
-		data = make([]float32, 0, cols*rows)
+		data = make([]float64, 0, cols*rows)
 	}
 	return Matrix{
 		Cols: cols,
 		Rows: rows,
 		Data: data,
 	}
+}
+
+func dot(a, b []float64) float64 {
+	sum := 0.0
+	for i, value := range a {
+		sum += value * b[i]
+	}
+	return sum
 }
 
 // MulT multiplies two matrices and computes the transpose
@@ -49,20 +55,20 @@ func (m Matrix) MulT(n Matrix) Matrix {
 	o := Matrix{
 		Cols: m.Rows,
 		Rows: n.Rows,
-		Data: make([]float32, 0, m.Rows*n.Rows),
+		Data: make([]float64, 0, m.Rows*n.Rows),
 	}
 	lenn, lenm := len(n.Data), len(m.Data)
 	for i := 0; i < lenn; i += columns {
 		nn := n.Data[i : i+columns]
 		for j := 0; j < lenm; j += columns {
 			mm := m.Data[j : j+columns]
-			o.Data = append(o.Data, vector.Dot(mm, nn))
+			o.Data = append(o.Data, dot(mm, nn))
 		}
 	}
 	return o
 }
 
-// Add adds two float32 matrices
+// Add adds two float64 matrices
 func (m Matrix) Add(n Matrix) Matrix {
 	lena, lenb := len(m.Data), len(n.Data)
 	if lena%lenb != 0 {
@@ -72,7 +78,7 @@ func (m Matrix) Add(n Matrix) Matrix {
 	o := Matrix{
 		Cols: m.Cols,
 		Rows: m.Rows,
-		Data: make([]float32, 0, m.Cols*m.Rows),
+		Data: make([]float64, 0, m.Cols*m.Rows),
 	}
 	for i, value := range m.Data {
 		o.Data = append(o.Data, value+n.Data[i%lenb])
@@ -81,9 +87,9 @@ func (m Matrix) Add(n Matrix) Matrix {
 }
 
 // Softmax calculates the softmax of the matrix rows
-func (m Matrix) Softmax(T float32) Matrix {
+func (m Matrix) Softmax(T float64) Matrix {
 	output := NewMatrix(m.Cols, m.Rows)
-	max := float32(0.0)
+	max := 0.0
 	for _, v := range m.Data {
 		v /= T
 		if v > max {
@@ -92,10 +98,10 @@ func (m Matrix) Softmax(T float32) Matrix {
 	}
 	s := max * S
 	for i := 0; i < len(m.Data); i += m.Cols {
-		sum := float32(0.0)
-		values := make([]float32, m.Cols)
+		sum := 0.0
+		values := make([]float64, m.Cols)
 		for j, value := range m.Data[i : i+m.Cols] {
-			values[j] = exp(value/T - s)
+			values[j] = math.Exp(value/T - s)
 			sum += values[j]
 		}
 		for _, value := range values {
@@ -109,9 +115,9 @@ func (m Matrix) Softmax(T float32) Matrix {
 func (m Matrix) Entropy() Matrix {
 	output := NewMatrix(m.Rows, 1)
 	for i := 0; i < len(m.Data); i += m.Cols {
-		entropy := float32(0.0)
+		entropy := 0.0
 		for _, value := range m.Data[i : i+m.Cols] {
-			entropy += value * log(value)
+			entropy += value * math.Log(value)
 		}
 		output.Data = append(output.Data, -entropy)
 	}
@@ -123,7 +129,7 @@ func (m Matrix) T() Matrix {
 	o := Matrix{
 		Cols: m.Rows,
 		Rows: m.Cols,
-		Data: make([]float32, 0, m.Cols*m.Rows),
+		Data: make([]float64, 0, m.Cols*m.Rows),
 	}
 	for i := 0; i < m.Cols; i++ {
 		for j := 0; j < m.Rows; j++ {
@@ -134,31 +140,31 @@ func (m Matrix) T() Matrix {
 }
 
 // AddRow adds a row to a matrix
-func (m Matrix) AddRow(row []float32) Matrix {
+func (m Matrix) AddRow(row []float64) Matrix {
 	if len(row) != m.Cols {
 		panic("incorrect number of columns")
 	}
 	o := Matrix{
 		Cols: m.Cols,
 		Rows: m.Rows + 1,
-		Data: make([]float32, m.Cols*m.Rows),
+		Data: make([]float64, m.Cols*m.Rows),
 	}
 	copy(o.Data, m.Data)
 	o.Data = append(o.Data, row...)
 	return o
 }
 
-func softmax(values []float32) {
-	max := float32(0.0)
+func softmax(values []float64) {
+	max := 0.0
 	for _, v := range values {
 		if v > max {
 			max = v
 		}
 	}
 	s := max * S
-	sum := float32(0.0)
+	sum := 0.0
 	for j, value := range values {
-		values[j] = exp(value - s)
+		values[j] = math.Exp(value - s)
 		sum += values[j]
 	}
 	for j, value := range values {
@@ -167,38 +173,38 @@ func softmax(values []float32) {
 }
 
 // SelfAttention computes the self attention of Q, K, V
-func SelfAttention(input Matrix) []float32 {
-	values := make([]float32, input.Rows)
+func SelfAttention(input Matrix) []float64 {
+	values := make([]float64, input.Rows)
 	V := input.T()
-	output := make([]float32, input.Cols)
+	output := make([]float64, input.Cols)
 	for i := 0; i < input.Rows; i++ {
 		K := input.Data[i*input.Cols : (i+1)*input.Cols]
 		for j := 0; j < input.Rows; j++ {
 			Q := input.Data[j*input.Cols : (j+1)*input.Cols]
-			values[j] = vector.Dot(K, Q)
+			values[j] = dot(K, Q)
 		}
 		softmax(values)
 
 		for j := 0; j < V.Rows; j++ {
 			V := V.Data[j*V.Cols : (j+1)*V.Cols]
-			output[j] += vector.Dot(values, V)
+			output[j] += dot(values, V)
 		}
 	}
-	aa := sqrt(vector.Dot(output[:], output[:]))
+	aa := math.Sqrt(dot(output[:], output[:]))
 	for i, v := range output {
 		output[i] = v / aa
 	}
 	return output
 }
 
-// CS is float32 normalized cosine similarity
-func CS(a []float32, b []float32) float32 {
-	aa, bb, ab := vector.Dot(a, a), vector.Dot(b, b), vector.Dot(a, b)
+// CS is float64 normalized cosine similarity
+func CS(a []float64, b []float64) float64 {
+	aa, bb, ab := dot(a, a), dot(b, b), dot(a, b)
 	if aa <= 0 {
 		return 0
 	}
 	if bb <= 0 {
 		return 0
 	}
-	return ab / (sqrt(aa) * sqrt(bb))
+	return ab / (math.Sqrt(aa) * math.Sqrt(bb))
 }
